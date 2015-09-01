6e3943d29aa542f881c581eacfa2bccb53a1bdd6
Till Sanders <tillsanders@googlemail.com>
2015-09-01 13:01:12 +0200

    First commit
    bringing some basic structure
    
    Signed-off-by: Till Sanders <tillsanders@googlemail.com>

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <title></title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <link rel="icon" href="favicon.png" />
+        <meta name="application-name" content="" />
+        <meta name="msapplication-TileImage" content="tile.png" />
+        <meta name="msapplication-square150x150logo" content="tile-150x150.png" />
+        <meta name="msapplication-square310x310logo" content="tile-310x310.png" />
+        <meta name="msapplication-square70x70logo" content="tile-70x70.png" />
+        <meta name="msapplication-wide310x150logo" content="tile-310x150.png" />
+        <meta name="msapplication-TileColor" content="#FF0000" />
+        <link rel="apple-touch-startup-image" href="launch6.png" media="(device-width: 375px)" />
+        <link rel="apple-touch-startup-image" href="launch6plus.png" media="(device-width: 414px)" />
+        <link rel="icon" sizes="any" mask href="pinned.svg" />
+        <meta name="theme-color" content="#000" />
+        <link rel="icon" sizes="196×196" href="favicon-196x196.png" />
+
+        <style>
+            <?php include('styles/css/base.css'); ?>
+        </style>
+        <link rel="stylesheet" media="screen, projection" href="styles/css/screen.css" />
+        <link rel="stylesheet" media="print" href="styles/css/print.css" />
+        <script src="scripts/vendor/modernizr.min.js"></script>
+    </head>
+    <body>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="scripts/vendor/jquery.min.js"><\/script>')</script>
+        <script src="scripts/plugins.pack.js"></script>
+        <script src="scripts/main.min.js"></script>
+    </body>
+</html>
