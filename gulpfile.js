7679c8f95dd59ae1fe77b2f52fb0deb94471e092
Till Sanders <tillsanders@googlemail.com>
2015-10-13 16:27:58 +0200

    Introduced cli flag for production mode
    Signed-off-by: Till Sanders <tillsanders@googlemail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,30 @@
 var gulp = require('gulp'),
 	sass = require('gulp-ruby-sass'),
 	notify = require('gulp-notify'),
-	livereload = require('gulp-livereload');
+	livereload = require('gulp-livereload'),
+	argv = require('yargs').argv;
 
 // Set directories
 var styles = 'styles/scss/**/*.scss';
 
+// Profiles
+var profiles = {
+	'development': {
+		'sass': {
+			'style': 'nested'
+		}
+	},
+	'production': {
+		'sass': {
+			'style': 'compressed'
+		}
+	}
+};
+var settings = (argv.production === undefined) ? profiles.development : profiles.production;
+
 // Task: Compile sass to css
 gulp.task('styles', function() {
-	return sass(styles, { style: 'expanded' })
+	return sass(styles, { style: settings.sass.style })
 		.pipe(gulp.dest('styles/css'))
 		.pipe(notify({
 			title: 'Styles updated!',
